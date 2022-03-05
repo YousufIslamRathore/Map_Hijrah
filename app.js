88d6132af7f0284c1160ac83cd3541b8385af333
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-03-05 16:18:08 +0500

    Updated Work

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var MenuItems = document.getElementById("MenuItems");
 
 MenuItems.style.maxHeight = "0px";
 
-
 function menutoggle() {
     if (MenuItems.style.maxHeight == "0px") {
         MenuItems.style.maxHeight = "270px";
